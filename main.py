c6f0445dedf7b72d5abc82befc077fae6104139b
NarMadat <narmadatian@gmail.com>
2025-12-18 20:20:57 +0400

    initialize logger and log game start in main function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.ship_input import get_player_ships
+from src.logger import setup_logger
+
+logger = setup_logger()
 
 def main():
+    logger.info("Game started")
     get_player_ships()
     generate_bot_ships()
 
